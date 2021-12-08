3d05170df77b3e6364bc2036158624c8f5fba6f6
Sanjay <sanjay883851@gmail.com>
2021-12-08 11:33:37 +0530

    portfolio

---
+++ src/pages/projectsdata.js
@@ -0,0 +1,19 @@
+const projectsdata = [
+    {
+      title: "SANJAY",
+      image: "./ecommerce.svg",
+      description:
+        "I am sanjay from kamaraj college of engineering and technology",
+      link: "https://www.youtube.com/",
+    },
+    {
+      title: "karthik",
+      image: "./pizzas.svg",
+      description:
+        "I am karthik from kamaraj college of engineering and technology",
+      link: "https://www.google.com/",
+    },
+    
+  ];
+
+  export default projectsdata
